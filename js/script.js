44e1ca4773ba37340a955caf13060394fc62b143
Andrea-Sgura <andreasgura96@gmail.com>
2025-03-25 02:31:13 +0100

    [UPD] Esercizio 2 - Definizione di funzione che genera numeri

--- js/script.js
+++ js/script.js
@@ -25,3 +25,10 @@ checkPalindroma(word);
 
 // Esercizio 2 - Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+// Definizione di funzione che genera un numero random da 1 a 5.
+function generateRandomNumber(){
+    return Math.floor(Math.random() * 5) + 1
+}
+
+
